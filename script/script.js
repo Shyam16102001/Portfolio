aa972f7370ed2f5bca831464304780306b380116
Shyam16102001 <55325014+Shyam16102001@users.noreply.github.com>
2022-12-25 12:27:21 +0530

    Add Certification Page

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ let navbar = document.querySelector('.nav_menu');
 let close = document.querySelector('.nav_close');
 
 menu.onclick = () => {
-  navbar.classList.add('show-menu');
+  navbar.classList.toggle('show-menu');
 };
 
 close.onclick = () => {
@@ -12,4 +12,44 @@ close.onclick = () => {
 
 window.onscroll = () => {
   navbar.classList.remove('show-menu');
+  document
+    .querySelector('.certification_model1')
+    .classList.remove('active-model');
+  document
+    .querySelector('.certification_model2')
+    .classList.remove('active-model');
+  document
+    .querySelector('.certification_model3')
+    .classList.remove('active-model');
+  document
+    .querySelector('.certification_model4')
+    .classList.remove('active-model');
 };
+
+function toggle(number) {
+  switch (number) {
+    case 1:
+      document
+        .querySelector('.certification_model1')
+        .classList.toggle('active-model');
+      break;
+    case 2:
+      document
+        .querySelector('.certification_model2')
+        .classList.toggle('active-model');
+      break;
+    case 3:
+      document
+        .querySelector('.certification_model3')
+        .classList.toggle('active-model');
+      break;
+    case 4:
+      document
+        .querySelector('.certification_model4')
+        .classList.toggle('active-model');
+      break;
+
+    default:
+      break;
+  }
+}
